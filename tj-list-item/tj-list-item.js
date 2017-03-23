13495eceff38e4778e6f2866a14eb1583a158e96
Jesper Johnsson <jesper.johnsson1995@hotmail.com>
2017-03-23 14:58:38 +0100

    Added the structure of the tj-list and tj-list-item Web Components

---
+++ tj-list-item/tj-list-item.js
@@ -0,0 +1,22 @@
+class TJListItem extends HTMLElement {
+
+  constructor() {
+    super(); // always call super() first in the ctor.
+
+  }
+
+  connectedCallback() {
+
+  }
+
+  disconnectedCallback() {
+
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+
+  }
+}
+
+customElements.define('tj-list-item', TJListItem);
+
